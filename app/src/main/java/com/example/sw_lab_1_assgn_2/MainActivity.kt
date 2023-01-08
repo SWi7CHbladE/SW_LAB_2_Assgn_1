28eda1c858fac4475d0698888a8f7789c418f9a2
Rahu_G <rahulgogoi.gogoi70@gmail.com>
2023-01-08 23:44:39 +0530

    Merge branch 'Developer' of https://github.com/SWi7CHbladE/SW_Lab_2_Assgn_1 into Developer

--- app/src/main/java/com/example/sw_lab_1_assgn_2/MainActivity.kt
+++ app/src/main/java/com/example/sw_lab_1_assgn_2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.sw_lab_1_assgn_2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button = findViewById<Button>(R.id.Process)
+        var output = findViewById<TextView>(R.id.Output)
+        var input = findViewById<EditText>(R.id.Message)
+
+
+        button.setOnClickListener {
+
+
+            output.text = "Number of Vowels = 5"
+        }
+
     }
 }
